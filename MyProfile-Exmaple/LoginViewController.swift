642ba6d321ce74fd2f5b7ae0c6caf467f0182793
Saleh I. D <Saleh.dhobaie@gmail.com>
2018-09-04 19:10:36 +0300

    adding more TODo

--- MyProfile-Exmaple/LoginViewController.swift
+++ MyProfile-Exmaple/LoginViewController.swift
@@ -9,26 +9,46 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginAction(_ sender: UIButton) {
+        
+        
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
+
+
+
+/*
+ * TODO : Here please add and implement `UITextFieldDelegate` function in order to validate Username/Password. username should be 4+ chars, password should be 8 chars
+ *
+ */
